f5a413baad097c9e984df0289f6607939d13e575
Wxj995-1 <za00011@163.com>
2026-03-19 15:58:37 +0800

    修改sort算法位快排算法

--- main.cpp
+++ main.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 using namespace std;
 
+void sort(int arr[],int i, int j)
+{
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while(l < r)
+	{
+		while(l < r && arr[r] >= val)
+		{
+			r--;
+		}
+		if(l < r)
+		{
+			arr[l++] = arr[r];
+		}
+		while(l < r && arr[l] <= val)
+		{
+			l++;
+		}
+		if(l < r)
+		{
+			arr[r--] = arr[l];
+		}
+
+	}
+	arr[l] = val;
+	sort(arr,i,l-1);
+	sort(arr,l+1,j);
+
+}
+
+
+
 
 int sort(int arr[],int size)
 {
-	int tmp = 0;
-	bool flag = false;
-	for(int i = 0;i < size -1;i++)
-	{
-		for(int j = 0;j<size-1-i;j++)
-		{
-			//Wxj996 修改排序算法 从大到小的排序
-			if(arr[j] < arr[j+1])
-			{
-				tmp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = tmp;
-				flag = true;
-			}
-		
-		}
-		if(!flae)
-			break;
-	}
-
-
-
+	sort(arr,0,size);
 }
 
 //Wxj995-1添加的注释
@@ -37,6 +49,11 @@ int main()
 	int arr[] = {12,4,5,6,7,88,888};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	sort(arr,size);
+	for(int i = 0;i< size;i++)
+	{
+		cout << arr[i] << "";
+	}
 
+	cout << endl;
 	return 0;
 }
