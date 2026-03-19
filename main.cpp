cf3ac89fdaa87ae7a48db429cd8a1dd8d98b747a
Ubuntu995 <za00011@163.com>
2026-03-19 14:01:54 +0800

    实现Sort

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+using namespace std;
+
+
+int sort(int arr[],int size)
+{
+	int tmp = 0;
+	for(int i = 0;i < size -1;i++)
+	{
+		for(int j = 0;j<size-1-i;j++)
+		{
+			if(arr[j] > arr[j+1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			
+			}
+		
+		}
+	}
+
+
+
+}
+
 
 
 
 int main()
 {
+	int arr[] = {12,4,5,6,7,88,888};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
 
 	return 0;
 }
